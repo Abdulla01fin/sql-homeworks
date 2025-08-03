bfd93e0fb75f8e99c3b82e647562c2744f5d9b99
Abdulla01fin <abdullaxodjayev7@gmail.com>
2025-08-03 11:18:21 +0500

    Create hw.sql

---
+++ lesson-1/homework/hw.sql
@@ -0,0 +1,5 @@
+create database SchoolDB
+use SchoolDB
+create table SchoolDB (student_ID int, name varchar (50), age int)
+insert into SchoolDB values (1,'a',25),(2,'b',24),(3,'v',22)
+select * from SchoolDB
